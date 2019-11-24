16aa7dd6862b994afbcc5b83362d589caa981eb2
Zak Johnson <fleecemane.lyons@icloud.com>
2019-11-23 18:23:37 -0800

    added tuple switch for rotating peices

--- pages/index.js
+++ pages/index.js
@@ -84,6 +84,29 @@ const playerMove = direction => {
     }
 }
 
+
+const rotate = (matrix, direction) => {
+    for (let y = 0; y < matrix.length; y++ ) {
+        for (let x = 0; x < y; x++) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x], 
+                matrix[x][y],
+            ]
+        }
+    }
+
+    if (direction > 0) {
+        matrix.forEach(row => row.reverse())
+    } else {
+        matrix.reverse();
+    }
+
+}
+
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
